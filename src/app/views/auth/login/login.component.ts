0fcdc3990964714a0ef4a6a9e8e36df69043c931
Sillfer <gliouinemahdi0@gmail.com>
2023-01-14 22:54:36 +0100

    auth done

---
+++ src/app/views/auth/login/login.component.ts
@@ -0,0 +1,14 @@
+import {Component, ErrorHandler, OnInit} from '@angular/core';
+import {AuthService} from "../../../_services/auth.service";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+
+}
